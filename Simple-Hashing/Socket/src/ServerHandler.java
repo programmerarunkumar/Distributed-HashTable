e095b0bc5e970f32517ebe8f49ae2114adbdc4cd
arunkumar.mp <arunkumar.mp@zohocorp.com>
2023-03-16 22:39:07 +0530

    Package Refactor

--- Simple-Hashing/Socket/src/ServerHandler.java
+++ Simple-Hashing/Socket/src/ServerHandler.java
@@ -1,3 +1,5 @@
+package src;
+
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
--- Simple-Hashing/Socket/src/ServerNode.java
+++ Simple-Hashing/Socket/src/ServerNode.java
@@ -1,3 +1,5 @@
+package src;
+
 public class ServerNode {
 
     private String ip;
